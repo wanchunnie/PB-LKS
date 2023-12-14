437444298b48042868b36f1af4a861d3095848b4
dh <19302010002@fudan.edu.cn>
2023-12-14 15:04:23 +0800

    minor modification

--- make_tree_plots.py
+++ make_tree_plots.py
@@ -5,6 +5,7 @@ import pickle
 import matplotlib.pyplot as plt
 
 RF_PATH = './PBLKS/PBLKS_model.pkl'
+DPI = 900
 
 clf = pickle.load(open(RF_PATH, 'rb'))
 # feature names & categories
@@ -13,7 +14,7 @@ cn = ['0(not host)', '1(is host)']
 
 # visualize decision tree into folder 1 by 1
 for i in range(len(clf.estimators_)):
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(10, 10), dpi=1000)
+    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(10, 10), dpi=DPI)
     tree.plot_tree(clf.estimators_[i],
                    feature_names=fn,
                    class_names=cn,
